18ab8c4ce36ae207d558303c96631cf5584257e0
Tapio Heiskanen <59442255+oh2th@users.noreply.github.com>
2022-12-28 09:50:04 +0200

    Minor cleanup

--- drivers/go-eCharger_V1/device.js
+++ drivers/go-eCharger_V1/device.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mainDevice = require('../main-device');
-const mainDriver = require('../main-driver');
 
 module.exports = class goeChargerDevice extends mainDevice {
 
--- drivers/go-eCharger_V2/driver.js
+++ drivers/go-eCharger_V2/driver.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Homey = require('homey');
 const mainDriver = require('../main-driver');
 
 module.exports = class goeChargerDriver extends mainDriver {
