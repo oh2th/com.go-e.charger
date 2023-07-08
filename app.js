7095105e6e30099610f748cb41fe245b70969c92
Tapio Heiskanen <tapio.heiskanen@iki.fi>
2023-07-08 08:33:42 +0300

    ADD: Sentry logging for uncaughtRejections.

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 'use strict';
 
 const Homey = require('homey');
+const { Log } = require('homey-log');
 const flowActions = require('./lib/flows/actions');
 const flowConditions = require('./lib/flows/conditions');
 
 class GoeCharger extends Homey.App {
 
-  /**
+	/**
    * onInit is called when the app is initialized.
    */
-  async onInit() {
-    this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
+	async onInit() {
+		this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} started...`);
+		this.homeyLog = new Log({ homey: this.homey });
 
-    await flowActions.init(this.homey);
-    await flowConditions.init(this.homey);
+		await flowActions.init(this.homey);
+		await flowConditions.init(this.homey);
 
-    this.log('go-eCharger app has been initialized');
-  }
+		this.log('go-eCharger app has been initialized');
+	}
 
-  async sendNotification(message) {
-    await this.homey.notifications.createNotification({
-      excerpt: message,
-    }).catch((error) => {
-      this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} sendNotification - error: ${error}`);
-    });
-  }
+	async sendNotification(message) {
+		await this.homey.notifications.createNotification({
+			excerpt: message,
+		}).catch((error) => {
+			this.log(`${this.homey.manifest.id} - ${this.homey.manifest.version} sendNotification - error: ${error}`);
+		});
+	}
 
 }
 
